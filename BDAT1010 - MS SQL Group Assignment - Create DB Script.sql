e856e81606adf0c78a2e13ef29268c75f0680df9
nicole_helf-admin <nicole.helf@mygeorgian.ca>
2024-07-05 11:29:07 -0400

    Added the Movie_Director table and inserted 30 records

--- BDAT1010 - MS SQL Group Assignment - Create DB Script.sql
+++ BDAT1010 - MS SQL Group Assignment - Create DB Script.sql
@@ -41,46 +41,47 @@ DROP TABLE IF EXISTS mov.Movies;
 DROP TABLE IF EXISTS mov.Movie_Actor;
 DROP TABLE IF EXISTS mov.Movie_Rating;
 
---Create the Movie_Director table
+--Create the Movie_Director table with constraints and specifications
 CREATE TABLE mov.Movie_Director(
-	Director_ID	int NOT NULL IDENTITY(100,10) PRIMARY KEY CLUSTERED,--what is a clustered index?
+	Director_ID	int NOT NULL IDENTITY(100,10) PRIMARY KEY CLUSTERED,
 	Director_First_Name varchar(50),
 	Director_Last_Name varchar(50),
 	Director_Age_in_Years int,
-	Director_Gender varchar,
+	Director_Gender varchar(10),
 	CreatedOn date NOT NULL DEFAULT GETDATE()
 );
 GO
 
+--insert values into Movie_Director table. Did not add Director_ID or CreatedOn values as they are auto generated from constraints.
 INSERT INTO mov.Movie_Director (Director_First_Name, Director_Last_Name, Director_Age_in_Years, Director_Gender)
-VALUES (
-	'Kevin', 'Smith', 52, 'Male',
-	'Miguel', 'Arteta', 41, 'Male',
-	'Mark', 'Johnson', 58, 'Male',
-	'Tom', 'Vaughan', 37, 'Male',
-	Francis Lawrence 52 Male
-	Adrienne Shelly 40 Female
-	David Slade 53 Male
-	Mark Palansky 53 Male
-	Jeff Lowell 49 Male
-	Simon Curtis 37 Male
-	Marc Lawrence 95 Male
-	Anand Tucker 59 Male
-	Judd Apatow 55 Male
-	Cary Fukunaga 45 Male
-	Mark Helfrich 49 Male
-	Nanette Burstein 52 Female
-	James McAvoy 44 Male
-	Mark Waters 58 Male
-	Seth Gordon 46 Male
-	Alex Kendrick 52 Male
-	Kevin Lima 60 Male
-	Lasse Hallström 76 Male
-	Ewan McGregor 52 Male
-	Rajkumar Hirani 60 Male
-	Ashutosh Gowariker 59 Male
-	Karan Johar 50 Male
-	S.S Rajamouli 49 Male
-	Sukumar NULL 53 Male
-	Aditya Chopra 51 Male
-	Umesh Shukla 52 Male
+VALUES 
+    ('Kevin', 'Smith', 52, 'Male'),
+    ('Miguel', 'Arteta', 41, 'Male'),
+    ('Mark', 'Johnson', 58, 'Male'),
+    ('Tom', 'Vaughan', 37, 'Male'),
+    ('Francis', 'Lawrence', 52, 'Male'),
+    ('Adrienne', 'Shelly', 40, 'Female'),
+    ('David', 'Slade', 53, 'Male'),
+    ('Mark', 'Palansky', 53, 'Male'),
+    ('Jeff', 'Lowell', 49, 'Male'),
+    ('Simon', 'Curtis', 37, 'Male'),
+    ('Marc', 'Lawrence', 95, 'Male'),
+    ('Anand', 'Tucker', 59, 'Male'),
+    ('Judd', 'Apatow', 55, 'Male'),
+    ('Cary', 'Fukunaga', 45, 'Male'),
+    ('Mark', 'Helfrich', 49, 'Male'),
+    ('Nanette', 'Burstein', 52, 'Female'),
+    ('James', 'McAvoy', 44, 'Male'),
+    ('Mark', 'Waters', 58, 'Male'),
+    ('Seth', 'Gordon', 46, 'Male'),
+    ('Alex', 'Kendrick', 52, 'Male'),
+    ('Kevin', 'Lima', 60, 'Male'),
+    ('Lasse', 'Hallström', 76, 'Male'),
+    ('Ewan', 'McGregor', 52, 'Male'),
+    ('Rajkumar', 'Hirani', 60, 'Male'),
+    ('Ashutosh', 'Gowariker', 59, 'Male'),
+    ('Karan', 'Johar', 50, 'Male'),
+    ('S.S', 'Rajamouli', 49, 'Male'),
+    ('Sukumar', NULL, 53, 'Male'),
+    ('Aditya', 'Chopra', 51, 'Male'),
+    ('Umesh', 'Shukla', 52, 'Male');
